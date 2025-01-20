369aeffbd977da3b977ad174f837bd7b1f73bc18
taegung <yesongmin12@gamil.com>
2025-01-20 15:06:37 +0900

    12 중첩 레이아웃 구성하기

---
+++ app/products/[productId]/layout.js
@@ -0,0 +1,8 @@
+export default function ProductDetailsLayout({children}) {
+    return (
+        <section>
+            {children}
+            <div className="p-4 bg-blue-500">상품 상세 페이지 CSS 디자인</div>
+        </section>
+    );
+}
