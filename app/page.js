1ad4181cc170a2d51b708dbb87141014a52b1899
taegung <yesongmin12@gamil.com>
2025-01-20 15:22:32 +0900

    link

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     // Shift + Alt + Left/Right key - 영역 선택/확장
-    <main>Hello Next!</main>
+    <>
+      <main>Hello Next!</main>
+      <Link href="/blog" className="text-2xl text-blue-500 underline">블로그</Link><br/>
+      <Link href="/products" className="text-2xl text-blue-500 underline">상품</Link>
+    </>
   );
 }
--- app/products/page.js
+++ app/products/page.js
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 const ProductList = () => {
+    const productList = [3, 4]; // 상품 목록이 들어있는 리스트
+
     return (
         <div>
             <h1>Product List</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <h2>
+                <Link href="products/1">Product 1</Link>
+            </h2>
+            <h2>
+                <Link href="products/2">Product 2</Link>
+            </h2>
+        
         </div>
     );
 }
